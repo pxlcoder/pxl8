2e02bfd4c62a4aebfbe4aa4005cefed2794af045
Aditya Keerthi <pxlcoder@gmail.com>
2016-10-12 16:15:44 -0400

    Load ROM into memory, begin work on InstructionSet

--- Sources/pxl8/Core/CPU.swift
+++ Sources/pxl8/Core/CPU.swift
@@ -1,30 +1,64 @@
+import Cocoa
+
 class CPU
 {
+    static let MEM_SIZE = 4096
+    static let PC_OFFSET = 512
+
     // 4K system memory
-    private var memory = [UInt8](repeating: 0, count: 4096)
+    internal var memory = [UInt8](repeating: 0, count: CPU.MEM_SIZE)
 
     // 16 8-bit registers, V0 - VF
     // VF is reserved for the carry flag
-    private var V = [UInt8](repeating: 0, count: 16)
+    internal var V = [UInt8](repeating: 0, count: 16)
 
     // The address register, used with several opcodes that involve memory operations
-    private var I: UInt16 = 0
+    internal var I: UInt16 = 0
 
     // Program counter, stores the address of the next instruction to execute
     // Begins at 0x200 as the original CHIP-8 interpreter occupied the first 512 bytes of memory
-    private var pc: UInt16 = 0x200
+    internal var pc: UInt16 = UInt16(CPU.PC_OFFSET)
 
     // 64x32 display - 1 represents on, 0 represents off
     public var display = Array(repeating: [Int](repeating: 0, count: 64), count: 32)
 
     // Two timer registers that count at 60 Hz
-    private var delayTimer: UInt8 = 0
-    private var soundTimer: UInt8 = 0
+    internal var delayTimer: UInt8 = 0
+    internal var soundTimer: UInt8 = 0
 
     // The stack, used to store return addresses when subroutines are called
-    private var stack = [UInt16](repeating:0, count: 16)
-    private var sp: Int = 0
+    internal var stack = [UInt16](repeating:0, count: 16)
+    internal var sp: Int = 0
 
     // Stores the current state of the keys, 16 in total
-    private var key = [UInt8](repeating:0, count:16)
+    internal var key = [UInt8](repeating:0, count:16)
+
+    // Current opcode
+    private var opcode: UInt16
+    {
+        return (UInt16(memory[pc]) << 8) | UInt16(memory[pc+1])
+    }
+
+    // TODO: Implement file open logic
+    init()
+    {
+        self.load(ROM: "/Users/akeerthi/Developer/pxl8/Resources/pong.rom")
+    }
+
+    func load(ROM: String)
+    {
+        let rom = fopen(ROM, "r")
+        fread(&memory + CPU.PC_OFFSET, MemoryLayout<UInt8>.size, CPU.MEM_SIZE - CPU.PC_OFFSET, rom)
+        fclose(rom)
+    }
+
+    func step()
+    {
+        print(String(format:"%04X", opcode))
+    }
+
+    func incrementPC()
+    {
+        pc += 2
+    }
 }
---
+++ Sources/pxl8/Core/InstructionSet.swift
@@ -0,0 +1,79 @@
+class InstructionSet
+{
+    // 0NNN - Calls RCA 1802 program at address NNN
+    func RCA(nnn: UInt16)
+    {
+        print("Warning! Not implemented.")
+    }
+
+    // 00E0 - Clears the screen
+    func CLR(_ cpu: CPU)
+    {
+        for row in 0..<32 {
+            for column in 0..<64 {
+                cpu.display[row][column] = 0
+            }
+        }
+    }
+
+    // 00EE - Returns from a subroutine
+
+    // 1NNN - Jumps to address NNN
+
+    // 2NNN - Calls subroutine at NNN
+
+    // 3XNN - Skips the next instruction if VX equals NN
+
+    // 4XNN - Skips the next instruction if VX doesn't equal NN
+
+    // 5XY0 - Skips the next instruction if VX equals VY
+
+    // 6XNN - Sets VX to NN
+
+    // 7XNN - Adds NN to VX
+
+    // 8XY0 - Sets VX to the value of VY
+
+    // 8XY1 - Sets VX to VX or VY
+
+    func OR(_ cpu: CPU, x: UInt8, y: UInt8)
+    {
+        cpu.V[x] = cpu.V[x] | cpu.V[y]
+    }
+
+    // 8XY2 - Sets VX to VX and VY
+
+    func AND(_ cpu: CPU, x: UInt8, y: UInt8)
+    {
+        cpu.V[x] = cpu.V[x] & cpu.V[y]
+    }
+
+    // 8XY3 - Sets VX to VX xor VY
+
+    func XOR(_ cpu: CPU, x: UInt8, y: UInt8)
+    {
+        cpu.V[x] = cpu.V[x] ^ cpu.V[y]
+    }
+
+    // 8XY4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+    // 8XY5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+    // 8XY6 - Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+    // 8XY7 - Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+    // 8XYE - Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
+    // 9XY0 - Skips the next instruction if VX doesn't equal VY
+    // ANNN - Sets I to the address NNN
+    // BNNN - Jumps to the address NNN plus V0
+    // CXNN - Sets VX to the result of a bitwise and operation on a random number and NN
+    // DXYN - Draws at sprite
+    // EX9E - Skips the next instruction if the key stored in VX is pressed
+    // EXA1 - Skips the next instruction if the key stored in VX isn't pressed
+    // FX07 - Sets VX to the value of the delay timer
+    // FX0A - A key press is awaited, and then stored in VX
+    // FX15 - Sets the delay timer to VX
+    // FX18 - Sets the sound timer to VX
+    // FX1E - Adds VX to I
+    // FX29 - Sets I to the location of the sprite for the character in VX
+    // FX33 - Stores the binary coded decimal representation of VX in I
+    // FX55 - Stores V0 to VX in memory starting at address I
+    // FX65 - Fills V0 to VX with values from memory starting at address I
+}
